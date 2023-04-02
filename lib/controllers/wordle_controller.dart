a42090f413d051f0b28675991fa194d9db423176
Mario Augusto de Freitas Nepomuceno <mg-guto@hotmail.com>
2023-04-02 17:54:56 -0300

    feature: Adicionando controller para cuidar do estado da tela

--- lib/controllers/wordle_controller.dart
+++ lib/controllers/wordle_controller.dart
@@ -1,15 +1,211 @@
+import 'dart:math';
+
+import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mali/helpers/string_helper.dart';
 
 import '../components/shaking.dart';
+import '../data/words.dart';
+import '../data/wordsall.dart';
+import '../helpers/colors_helper.dart';
+import '../models/enum/game_status.dart';
+import '../models/enum/letter_status.dart';
+import '../models/letter_model.dart';
+import '../models/word_model.dart';
 
 abstract class WordleListener {
-  void onLoginSuccess();
-  void onLoginError(String errorMessage);
+  void showSnackBar(Color color, String titleText, String buttonText);
 }
 
-class WordleController {
+class WordleController extends ChangeNotifier {
+  WordleListener? _listener;
+
+  void setListener(WordleListener listener) {
+    _listener = listener;
+  }
+
   final shakeKey = GlobalKey<ShakeWidgetState>();
   final focusNode = FocusNode();
 
+  GameStatus gameStatus = GameStatus.playing;
   int currentWordIndex = 0;
+
+  final List<Word> board = List.generate(
+    6,
+    (_) => Word(
+      letters: List.generate(
+        5,
+        (_) => Letter.empty(),
+      ),
+    ),
+  );
+
+  Word? get currentWord =>
+      currentWordIndex < board.length ? board[currentWordIndex] : null;
+
+  Word solution = Word.fromString(
+    fiveLetterWords[Random().nextInt(fiveLetterWords.length)].toUpperCase(),
+  );
+
+  final List<List<GlobalKey<FlipCardState>>> flipCardKeys = List.generate(
+    6,
+    (_) => List.generate(
+      5,
+      (_) => GlobalKey<FlipCardState>(),
+    ),
+  );
+
+  final Set<Letter> keyboardLetters = {};
+
+  void onKeyTapped(String key) {
+    if (gameStatus == GameStatus.playing) {
+      currentWord?.addLetter(key);
+      notifyListeners();
+    }
+  }
+
+  void onDeleteTapped() {
+    if (gameStatus == GameStatus.playing) {
+      currentWord?.removeLetter();
+      notifyListeners();
+    }
+  }
+
+  Future<void> onEnterTapped() async {
+    String exists = fiveLetterWordsAll.firstWhere(
+        (element) =>
+            element.removeSpecials() ==
+            currentWord!.wordString.removeSpecials(),
+        orElse: () => "");
+
+    if (exists != "") {
+      if (gameStatus == GameStatus.playing &&
+          currentWord != null &&
+          !currentWord!.letters.contains(Letter.empty())) {
+        gameStatus = GameStatus.submitting;
+
+        for (var i = 0; i < currentWord!.letters.length; i++) {
+          final currentWordLetter = currentWord!.letters[i];
+          final currentSolutionLetter = solution.letters[i];
+
+          if (currentWordLetter.val.removeSpecials() ==
+              currentSolutionLetter.val.removeSpecials()) {
+            currentWord!.letters[i] = currentWordLetter.copyWith(
+              val: currentSolutionLetter.val,
+              status: LetterStatus.correct,
+            );
+          } else if (solution.letters.contains(currentWordLetter)) {
+            if (solution.letters
+                    .where((element) => element.val == currentWordLetter.val)
+                    .length ==
+                currentWord!.letters
+                    .where((element) =>
+                        element.val == currentWordLetter.val &&
+                        element.status == LetterStatus.correct)
+                    .length) {
+              currentWord!.letters[i] =
+                  currentWordLetter.copyWith(status: LetterStatus.notInWord);
+            } else {
+              currentWord!.letters[i] =
+                  currentWordLetter.copyWith(status: LetterStatus.inWord);
+            }
+          } else {
+            currentWord!.letters[i] =
+                currentWordLetter.copyWith(status: LetterStatus.notInWord);
+          }
+
+          notifyListeners();
+
+          final letter = keyboardLetters.firstWhere(
+              (e) => e.val == currentWordLetter.val,
+              orElse: () => Letter.empty());
+          if (letter.status != LetterStatus.correct) {
+            keyboardLetters.removeWhere((e) => e.val == currentWordLetter.val);
+            keyboardLetters.add(currentWord!.letters[i]);
+          }
+
+          await Future.delayed(
+            Duration(milliseconds: i * 150),
+            () => flipCardKeys[currentWordIndex][i].currentState?.toggleCard(),
+          );
+        }
+
+        _checkIfWinOrLoss();
+      }
+    } else {
+      shakeKey.currentState?.shake();
+    }
+  }
+
+  void _checkIfWinOrLoss() {
+    if (currentWord!.wordString == solution.wordString) {
+      gameStatus = GameStatus.won;
+      _listener!.showSnackBar(correctColor,
+          'Você perdeu! Solução: ${solution.wordString}', "Novo Jogo");
+    } else if (currentWordIndex + 1 >= board.length) {
+      gameStatus = GameStatus.lost;
+      _listener!.showSnackBar(correctColor, "Você ganhou!", "Novo Jogo");
+    } else {
+      gameStatus = GameStatus.playing;
+    }
+
+    currentWordIndex += 1;
+    notifyListeners();
+  }
+
+  void restart() {
+    gameStatus = GameStatus.playing;
+    currentWordIndex = 0;
+    board
+      ..clear()
+      ..addAll(
+        List.generate(
+          6,
+          (_) => Word(
+            letters: List.generate(
+              5,
+              (_) => Letter.empty(),
+            ),
+          ),
+        ),
+      );
+    solution = Word.fromString(
+      fiveLetterWords[Random().nextInt(fiveLetterWords.length)].toUpperCase(),
+    );
+    keyboardLetters.clear();
+    flipCardKeys
+      ..clear()
+      ..addAll(
+        List.generate(
+          6,
+          (_) => List.generate(
+            5,
+            (_) => GlobalKey<FlipCardState>(),
+          ),
+        ),
+      );
+
+    notifyListeners();
+  }
+
+  handleKey(RawKeyEvent key) {
+    if (key.runtimeType.toString() == 'RawKeyDownEvent') {
+      if (key.character != null && key.character != "") {
+        onKeyTapped(key.character.toString().toUpperCase());
+      } else {
+        if (key.isKeyPressed(LogicalKeyboardKey.enter)) {
+          onEnterTapped();
+        } else if (key.isKeyPressed(LogicalKeyboardKey.backspace)) {
+          onDeleteTapped();
+        } else if (key.isKeyPressed(LogicalKeyboardKey.arrowRight)) {
+          board[currentWordIndex].goRight();
+          notifyListeners();
+        } else if (key.isKeyPressed(LogicalKeyboardKey.arrowLeft)) {
+          board[currentWordIndex].goLeft();
+          notifyListeners();
+        }
+      }
+    }
+  }
 }
--- lib/views/wordle/wordle_screen.dart
+++ lib/views/wordle/wordle_screen.dart
@@ -1,17 +1,9 @@
-import 'dart:math';
-
 import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import '../../controllers/wordle_controller.dart';
-import '../../data/words.dart';
-import '../../helpers/string_helper.dart';
-import '../../helpers/colors_helper.dart';
-import '../../models/enum/game_status.dart';
 import '../../models/enum/letter_status.dart';
 import '../../models/letter_model.dart';
-import '../../models/word_model.dart';
 import '../../components/shaking.dart';
 import 'components/board_tile.dart';
 import 'components/keyboard.dart';
@@ -23,303 +15,127 @@ class WordleScreen extends StatefulWidget {
   State<WordleScreen> createState() => _WordleScreenState();
 }
 
-class _WordleScreenState extends State<WordleScreen> {
-  final WordleController _wordleController = WordleController();
+class _WordleScreenState extends State<WordleScreen> implements WordleListener {
+  final WordleController wordleController = WordleController();
 
-  final _shakeKey = GlobalKey<ShakeWidgetState>();
-  final focusNode = FocusNode();
-
-  int _currentWordIndex = 0;
-  GameStatus _gameStatus = GameStatus.playing;
-
-  Word? get _currentWord =>
-      _currentWordIndex < _board.length ? _board[_currentWordIndex] : null;
-
-  Word _solution = Word.fromString(
-    fiveLetterWords[Random().nextInt(fiveLetterWords.length)].toUpperCase(),
-  );
-
-  final List<Word> _board = List.generate(
-    6,
-    (_) => Word(
-      letters: List.generate(
-        5,
-        (_) => Letter.empty(),
+  @override
+  void showSnackBar(Color color, String titleText, String buttonText) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        dismissDirection: DismissDirection.none,
+        duration: const Duration(days: 1),
+        backgroundColor: color,
+        content: Text(
+          titleText,
+          style: const TextStyle(color: Colors.white),
+        ),
+        action: SnackBarAction(
+          onPressed: wordleController.restart,
+          textColor: Colors.white,
+          label: buttonText,
+        ),
       ),
-    ),
-  );
-
-  final List<List<GlobalKey<FlipCardState>>> _flipCardKeys = List.generate(
-    6,
-    (_) => List.generate(
-      5,
-      (_) => GlobalKey<FlipCardState>(),
-    ),
-  );
-
-  final Set<Letter> _keyboardLetters = {};
-
-  void _onKeyTapped(String val) {
-    if (_gameStatus == GameStatus.playing) {
-      setState(() {
-        _currentWord?.addLetter(val);
-      });
-    }
-  }
-
-  void _onDeleteTapped() {
-    if (_gameStatus == GameStatus.playing) {
-      setState(() {
-        _currentWord?.removeLetter();
-      });
-    }
-  }
-
-  Future<void> _onEnterTapped() async {
-    String exists = fiveLetterWords.firstWhere(
-        (element) =>
-            element.removeSpecials() ==
-            _currentWord!.wordString.removeSpecials(),
-        orElse: () => "");
-
-    if (exists != "") {
-      if (_gameStatus == GameStatus.playing &&
-          _currentWord != null &&
-          !_currentWord!.letters.contains(Letter.empty())) {
-        _gameStatus = GameStatus.submitting;
-
-        for (var i = 0; i < _currentWord!.letters.length; i++) {
-          final currentWordLetter = _currentWord!.letters[i];
-          final currentSolutionLetter = _solution.letters[i];
-
-          setState(() {
-            if (currentWordLetter.val.removeSpecials() ==
-                currentSolutionLetter.val.removeSpecials()) {
-              _currentWord!.letters[i] = currentWordLetter.copyWith(
-                val: currentSolutionLetter.val,
-                status: LetterStatus.correct,
-              );
-            } else if (_solution.letters.contains(currentWordLetter)) {
-              if (_solution.letters
-                      .where((element) => element.val == currentWordLetter.val)
-                      .length ==
-                  _currentWord!.letters
-                      .where((element) =>
-                          element.val == currentWordLetter.val &&
-                          element.status == LetterStatus.correct)
-                      .length) {
-                _currentWord!.letters[i] =
-                    currentWordLetter.copyWith(status: LetterStatus.notInWord);
-              } else {
-                _currentWord!.letters[i] =
-                    currentWordLetter.copyWith(status: LetterStatus.inWord);
-              }
-            } else {
-              _currentWord!.letters[i] =
-                  currentWordLetter.copyWith(status: LetterStatus.notInWord);
-            }
-          });
-
-          final letter = _keyboardLetters.firstWhere(
-              (e) => e.val == currentWordLetter.val,
-              orElse: () => Letter.empty());
-          if (letter.status != LetterStatus.correct) {
-            _keyboardLetters.removeWhere((e) => e.val == currentWordLetter.val);
-            _keyboardLetters.add(_currentWord!.letters[i]);
-          }
-
-          await Future.delayed(
-            Duration(milliseconds: i * 150),
-            () =>
-                _flipCardKeys[_currentWordIndex][i].currentState?.toggleCard(),
-          );
-        }
-
-        _checkIfWinOrLoss();
-      }
-    } else {
-      _shakeKey.currentState?.shake();
-    }
-  }
-
-  void _checkIfWinOrLoss() {
-    if (_currentWord!.wordString == _solution.wordString) {
-      _gameStatus = GameStatus.won;
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          dismissDirection: DismissDirection.none,
-          duration: const Duration(days: 1),
-          backgroundColor: correctColor,
-          content: const Text(
-            'Você ganhou!',
-            style: TextStyle(color: Colors.white),
-          ),
-          action: SnackBarAction(
-            onPressed: _restart,
-            textColor: Colors.white,
-            label: 'Novo Jogo',
-          ),
-        ),
-      );
-    } else if (_currentWordIndex + 1 >= _board.length) {
-      _gameStatus = GameStatus.lost;
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          dismissDirection: DismissDirection.none,
-          duration: const Duration(days: 1),
-          backgroundColor: Colors.redAccent[200],
-          content: Text(
-            'Você perdeu! Solução: ${_solution.wordString}',
-            style: const TextStyle(color: Colors.white),
-          ),
-          action: SnackBarAction(
-            onPressed: _restart,
-            textColor: Colors.white,
-            label: 'Novo Jogo',
-          ),
-        ),
-      );
-    } else {
-      _gameStatus = GameStatus.playing;
-    }
-
-    setState(() {
-      _currentWordIndex += 1;
-    });
-  }
-
-  void _restart() {
-    setState(() {
-      _gameStatus = GameStatus.playing;
-      _currentWordIndex = 0;
-      _board
-        ..clear()
-        ..addAll(
-          List.generate(
-              6, (_) => Word(letters: List.generate(5, (_) => Letter.empty()))),
-        );
-      _solution = Word.fromString(
-        fiveLetterWords[Random().nextInt(fiveLetterWords.length)].toUpperCase(),
-      );
-      _keyboardLetters.clear();
-      _flipCardKeys
-        ..clear()
-        ..addAll(List.generate(
-            6, (_) => List.generate(5, (_) => GlobalKey<FlipCardState>())));
-    });
-  }
-
-  handleKey(RawKeyEvent key) {
-    if (key.runtimeType.toString() == 'RawKeyDownEvent') {
-      if (key.character != null && key.character != "") {
-        _onKeyTapped(key.character.toString().toUpperCase());
-      } else {
-        if (key.isKeyPressed(LogicalKeyboardKey.enter)) {
-          _onEnterTapped();
-        } else if (key.isKeyPressed(LogicalKeyboardKey.backspace)) {
-          _onDeleteTapped();
-        } else if (key.isKeyPressed(LogicalKeyboardKey.arrowRight)) {
-          setState(() {
-            _board[_currentWordIndex].goRight();
-          });
-        } else if (key.isKeyPressed(LogicalKeyboardKey.arrowLeft)) {
-          setState(() {
-            _board[_currentWordIndex].goLeft();
-          });
-        }
-      }
-      // print("why does this run twice $keyCode");
-    }
+    );
   }
 
   @override
   void initState() {
     super.initState();
+    wordleController.setListener(this);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          title: const Text(
-            'Termo',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 36,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 4,
-            ),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text(
+          'Termo',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 36,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 4,
           ),
         ),
-        body: RawKeyboardListener(
-          autofocus: true,
-          onKey: handleKey,
-          focusNode: FocusNode(),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              ShakeWidget(
-                key: _shakeKey,
-                shakeCount: 3,
-                shakeOffset: 10,
-                shakeDuration: const Duration(milliseconds: 400),
-                child: Column(
-                  children: _board
-                      .asMap()
-                      .map(
-                        (i, word) => MapEntry(
-                          i,
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: word.letters
-                                .asMap()
-                                .map((j, letter) => MapEntry(
-                                    j,
-                                    FlipCard(
-                                      key: _flipCardKeys[i][j],
-                                      flipOnTouch: false,
-                                      speed: 1000,
-                                      direction: FlipDirection.HORIZONTAL,
-                                      front: GestureDetector(
-                                        onTap: () {
-                                          setState(() {
-                                            word.changeLetter(j);
-                                          });
-                                        },
-                                        child: BoardTile(
-                                          letter: Letter(
-                                            val: letter.val,
-                                            status: LetterStatus.initial,
+      ),
+      body: AnimatedBuilder(
+          animation: wordleController,
+          builder: (context, snapshot) {
+            return RawKeyboardListener(
+              autofocus: true,
+              onKey: wordleController.handleKey,
+              focusNode: FocusNode(),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ShakeWidget(
+                    key: wordleController.shakeKey,
+                    shakeCount: 3,
+                    shakeOffset: 10,
+                    shakeDuration: const Duration(milliseconds: 400),
+                    child: Column(
+                      children: wordleController.board
+                          .asMap()
+                          .map(
+                            (i, word) => MapEntry(
+                              i,
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: word.letters
+                                    .asMap()
+                                    .map((j, letter) => MapEntry(
+                                        j,
+                                        FlipCard(
+                                          key: wordleController.flipCardKeys[i]
+                                              [j],
+                                          flipOnTouch: false,
+                                          speed: 1000,
+                                          direction: FlipDirection.HORIZONTAL,
+                                          front: GestureDetector(
+                                            onTap: () {
+                                              setState(() {
+                                                word.changeLetter(j);
+                                              });
+                                            },
+                                            child: BoardTile(
+                                              letter: Letter(
+                                                val: letter.val,
+                                                status: LetterStatus.initial,
+                                              ),
+                                              isSelected: word.currentIndex ==
+                                                      j &&
+                                                  wordleController
+                                                          .currentWordIndex ==
+                                                      i,
+                                            ),
                                           ),
-                                          isSelected: word.currentIndex == j &&
-                                              _currentWordIndex == i,
-                                        ),
-                                      ),
-                                      back: BoardTile(
-                                        letter: letter,
-                                        isSelected: false,
-                                      ),
-                                    )))
-                                .values
-                                .toList(),
-                          ),
-                        ),
-                      )
-                      .values
-                      .toList(),
-                ),
+                                          back: BoardTile(
+                                            letter: letter,
+                                            isSelected: false,
+                                          ),
+                                        )))
+                                    .values
+                                    .toList(),
+                              ),
+                            ),
+                          )
+                          .values
+                          .toList(),
+                    ),
+                  ),
+                  Keyboard(
+                    onKeyTapped: wordleController.onKeyTapped,
+                    onEnterTapped: wordleController.onEnterTapped,
+                    onDeleteTapped: wordleController.onDeleteTapped,
+                    letters: wordleController.keyboardLetters,
+                  ),
+                ],
               ),
-              Keyboard(
-                onKeyTapped: _onKeyTapped,
-                onEnterTapped: _onEnterTapped,
-                onDeleteTapped: _onDeleteTapped,
-                letters: _keyboardLetters,
-              ),
-            ],
-          ),
-        ));
+            );
+          }),
+    );
   }
 }
